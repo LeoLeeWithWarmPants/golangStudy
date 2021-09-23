f7df27d87bf3554a3cdd6900d48ace8b142216b6
LeoLee <512240816@qq.com>
2021-09-23 13:20:20 +0800

    1.golang中自定义类型的string()方法

--- project17method/README.md
+++ project17method/README.md
@@ -1,4 +1,4 @@
-Golang中的方法比较特殊，其作用在指定的数据类型上，所以自定义类型（通过type定义的类型）都可以定义方法，不仅仅是struct。
+Golang中的方法比较特殊，其作用在指定的数据类型上，所以自定义类型（通过type定义的类型）都可以定义方法，不仅仅是struct，**如type interger int，可以给这个integer绑定方法**。
 
 ```go
 type A struct {
@@ -14,8 +14,20 @@ func (a A) xxx() {
 - func (a A) xxx()为A结构的一个方法（A结构有一个方法为func (a A) xxx()）。
 - func (a A) xxx()与结构A是绑定的。
 
+方法定义的语法：
+
+```go
+func ([recevier] [type]) [methodName]([params...]) ([return list...]) {
+	//code
+	return ...
+}
+```
+
 注意：
 
 - **一个对象调用自己的方法时，会将对象自身作为参数传递给方法**。
 - **对象调用方法时，是将自身的副本作为参数进行传递的**。
+  - **如果需要进行引用传递，那么就需要将recevier的类型定义为指针**。
+- 方法的作用域控制规则与函数一致，方法名字母小写，只能在本包访问，方法名首字母大写，可以全局访问。
+- **如果一个类型实现了一个String()这个方法，那么fmt.Println默认会调用这个类型的这个String()方法**。相当于Java重写了toString()
 
--- project17method/demo1/main/main.go
+++ project17method/demo1/main/main.go
@@ -21,7 +21,13 @@ func (a A) Amethod2(i int) A {
 
 //使用指针进行引用传递
 func (a *A) Amethod3(i int) {
-	(*a).Id += i
+	fmt.Printf("a type:%T,a address:%p\n", a, a)
+	(*a).Id += i //golang由于编译器优化，也可以写为：a.Id += i
+}
+
+//自定义String()方法
+func (a *A) String() string {
+	return fmt.Sprintf("自定义String()方法执行，a=%v\n", *a)
 }
 
 func main() {
@@ -31,6 +37,17 @@ func main() {
 	a1.Amethod2(9)
 	fmt.Printf("a1:%v\n", *a1) //可以看出：对象调用方法的时候，会将自己的副本作为参数进行传递
 
-	a1.Amethod3(2) //使用指针作为方法的参数，就可以进行引用传递
-	fmt.Printf("a1:%v\n", *a1)
+	(*a1).Amethod3(2) //使用指针作为方法的参数，就可以进行引用传递，golang由于编译器优化，也可以写为：a1.Amethod3(2)
+	fmt.Printf("a1:%v, a1 address:%p\n", *a1, a1)
+
+	//如果一个类型实现了一个String()这个方法，那么fmt.Println默认会调用这个类型的这个String()方法，相当于Java重写了toString()
+	a1str := (*a1).String()
+	fmt.Printf("a1str=%s\n", a1str)
+	fmt.Printf("a1 type:%T\n", a1) //a1为指针类型
+	fmt.Println(a1)                //如果传递是指针，则调用了自定义的String()方法
+	fmt.Println(*a1)
+	a2 := A{Id: 2}
+	fmt.Printf("a2 type:%T\n", a2)
+	fmt.Println(a2)
+	fmt.Println(&a2) //如果传递是指针，则调用了自定义的String()方法
 }
