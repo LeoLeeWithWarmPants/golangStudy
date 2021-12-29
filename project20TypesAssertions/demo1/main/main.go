148a4dbf9bf882fe2b35946886b9bcbee8b3512c
LeoLee <512240816@qq.com>
2021-12-29 14:00:16 +0800

    1.可检测的类型断言

--- project20TypesAssertions/demo1/main/main.go
+++ project20TypesAssertions/demo1/main/main.go
@@ -21,4 +21,12 @@ func main() {
 	// c2 = e 编译错误：Cannot use 'e' (type emptyInterface) as the type Coordinate
 	c2 = e.(Coordinate) //类型断言，类似于Java中的类型强制转换，如果变量e不是Coordinate类型，那将会产生恐慌
 	fmt.Println(c2)
+
+	//可检测的类型断言
+	var e2 emptyInterface
+	var f1 float64 = 1.1
+	e2 = f1
+	y, isOk := e2.(float64)
+	fmt.Printf("e2类型断言：%t，", isOk)
+	fmt.Printf("y的类型是%T\n", y)
 }
