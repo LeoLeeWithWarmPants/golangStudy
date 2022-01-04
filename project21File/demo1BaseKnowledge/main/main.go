e1446bc9e6cce5e4e1a10dc491726e1900cd6eb0
LeoLee <512240816@qq.com>
2022-01-04 16:35:45 +0800

    1.怎么创建一个file对象

--- project21File/demo1BaseKnowledge/main/main.go
+++ project21File/demo1BaseKnowledge/main/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func openFile(filePath string) *os.File {
+	//打开文件
 	file, err := os.Open(filePath)
 	if err != nil {
 		err.Error()
@@ -13,7 +14,16 @@ func openFile(filePath string) *os.File {
 	return file
 }
 
-func main() {
+func baseTest() {
 	file := openFile("C:\\Users\\WEIMOB\\Desktop\\fileTestForGolang.txt")
 	fmt.Printf("type:%T, value:%v\n", file, file)
+	//关闭文件
+	err := file.Close()
+	if err != nil {
+		fmt.Printf("close file err:%v\n", err)
+	}
+}
+
+func main() {
+	baseTest()
 }
