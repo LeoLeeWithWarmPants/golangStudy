66b5cf2910b56ba78a05d587ca116e00386311a2
LeoLee <512240816@qq.com>
2022-01-08 18:40:17 +0800

    1.使用json.Marshal对struct进行json序列化

---
+++ project23JsonSerialization/demo1/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+type Student struct {
+	BaseInfo Person
+	Type     int // 1-小学生 2-中学生 3-大学生
+	Grade    string
+}
+
+func toJson(v interface{}) string {
+	jsonByte, err := json.Marshal(v)
+	if err != nil {
+		fmt.Printf("serialization failed, err:%v\n", err)
+	}
+	fmt.Printf("json result:%s\n", string(jsonByte))
+	return string(jsonByte)
+}
+
+func main() {
+	person := Person{Name: "LeoLee", Age: 18}
+	student := new(Student)
+	student.BaseInfo = person
+	student.Type = 3
+	student.Grade = "大一"
+	toJson(student)
+}
