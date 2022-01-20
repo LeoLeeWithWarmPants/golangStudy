14878ab1a3059ef20c540da1dd3742e019ce2097
LeoLee <512240816@qq.com>
2022-01-20 17:02:48 +0800

    1.空接口类型的channel(泛型channel)

--- project25goroutine/demo3Channel/main/main.go
+++ project25goroutine/demo3Channel/main/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golangStudyProject/project15OOP/demo1extends/models"
+)
 
 func main() {
 	//声明管道，使用前必须make
@@ -23,4 +26,29 @@ func main() {
 	num3 := <-intChan
 	fmt.Printf("num3=%d\n", num3)
 	//num4 := <-intChan //fatal error: all goroutines are asleep - deadlock!
+
+	//空接口类型的channel(泛型channel)
+	genericChan := make(chan interface{}, 3)
+	genericChan <- 1
+	genericChan <- "LeoLee"
+	//构建一个结构体，放入泛型channel
+	person1 := models.Person{Name: "LeoLee", Age: 27}
+	book1 := models.Book{Name: "golang从入门到放弃", Price: 100.02}
+	person1.Book = book1
+	person1.Price = 5000.02
+	brand1 := &models.Brand{Name: "apple"}
+	person1.Brand = brand1
+	fmt.Printf("person1 = %v, phone's brand = %v\n", person1, (*person1.Brand).Name)
+	genericChan <- person1
+	//获取channel中的结构体
+	<-genericChan //获取但是并不是赋值
+	<-genericChan
+	person2 := <-genericChan
+	fmt.Printf("person2的类型为=%T, person2=%v\n", person2, person2)
+	//能不能获取结构体的属性呢？？？
+	//fmt.Printf("person2.Name=%v\n", person2.Name) //编译就报错，并不能直接通过person2获取，编译的时候认为这个person2就是interface{}类型
+	//所以使用类型断言
+	person3 := person2.(models.Person)
+	fmt.Printf("person3.Name=%v\n", person3.Name)
+
 }
