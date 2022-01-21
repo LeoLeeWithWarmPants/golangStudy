a1d47d121ab4d41a2ca3a59b0a4710b080a7527b
LeoLee <512240816@qq.com>
2022-01-21 20:33:08 +0800

    1.计算分表索引的小工具

---
+++ exercises/exercise4GetHashCode/main/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func hashForJava(str string) int32 {
+	var hashCode int32 = 0
+	valueArray := strToCharArray(str)
+	if len(str) > 0 {
+		for i := 0; i < len(valueArray); i++ {
+			hashCode = 31*hashCode + int32(valueArray[i])
+		}
+	}
+	return hashCode
+}
+
+func strToCharArray(str string) []byte {
+	return []byte(str)
+}
+
+func getResult(strKey string, salt int32) int32 {
+	hashResult := hashForJava(strKey)
+	result := int32(math.Abs(float64(hashResult))) % salt
+	return result
+}
+
+func printStrKeyReminder() {
+	fmt.Println("喂！请输入分表键：")
+}
+
+func printSaltReminder() {
+	fmt.Println("喂！请输入分表数：")
+}
+
+func printResult(result int32) {
+	fmt.Println(result)
+}
+
+func printContinueText() {
+	fmt.Println("输入666继续计算")
+}
+
+func logic(strKey string, salt int32) {
+	var continueText string
+	for {
+		printStrKeyReminder()
+		fmt.Scanln(&strKey)
+		printSaltReminder()
+		fmt.Scanln(&salt)
+		printResult(getResult(strKey, salt))
+		printContinueText()
+		fmt.Scanln(&continueText)
+		if strings.Compare(continueText, "666") == 0 {
+
+		}
+	}
+}
+
+func main() {
+	var strKey string
+	var salt int32
+	logic(strKey, salt)
+}
