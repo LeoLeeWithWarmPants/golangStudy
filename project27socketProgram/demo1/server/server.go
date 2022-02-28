62ba8f2a7c693638a7a7dce7f7c1e7fee9ac2f30
LeoLee <512240816@qq.com>
2022-02-28 15:42:56 +0800

    1.tcp服务端监听客户端连接

--- project27socketProgram/demo1/server/server.go
+++ project27socketProgram/demo1/server/server.go
@@ -13,7 +13,9 @@ func startServer(protocol, address string, port int) {
 		fmt.Printf("listen error:%v\n", error)
 		panic(error)
 	}
+	fmt.Printf("listen successful, wait for client connect\n")
 	for {
+		//这里等待客户端链接会阻塞的，直到有客户端进行链接
 		conn, error := listener.Accept()
 		if error != nil {
 			fmt.Printf("accept error:%v\n", error)
@@ -22,4 +24,9 @@ func startServer(protocol, address string, port int) {
 			fmt.Printf("remote address[%v] connected success\n", conn.RemoteAddr())
 		}
 	}
+}
+
+func main() {
+	startServer("tcp", "0.0.0.0", 9528)
+	//启动后telnet 127.0.0.1 9528
 }
